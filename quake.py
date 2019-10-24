4b64601bbca2797d2caa7d889ee60816a4f73643
StefanPowell <Stefan.Powell001@umb.edu>
2019-10-24 19:36:21 -0400

    parse data

--- quake.py
+++ quake.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 
 keep_first_parse = []
 
@@ -8,14 +9,30 @@ mybytes = data.read()
 webpage_code = mybytes.decode("utf8")
 data.close()
 
-def web_partition(start, end):
-    for x in range(len(webpage_code)):  # get tables
-        if (webpage_code[x:x + 7] == "<tbody>"):
-            for j in range(x, len(webpage_code)):
-                keep_first_parse.append(webpage_code[j])
-                if (webpage_code[j - 8:j] == "<tbody>"):
+
+def web_partition(start, end, code):
+    for x in range(len(code)):  # get tables
+        if (code[x:x + len(start)] == start):
+            for j in range(x, len(code)):
+                keep_first_parse.append(code[j])
+                if (code[j - len(end):j] == end):
                     break
+    return keep_first_parse
 
 
-first_partition  = ''.join(keep_first_parse)
-print(first_partition )
+def clean_data(array_data, partition):
+    for y in range(len(array_data)):
+        partition = partition.replace(array_data[y], '')
+    return partition
+
+
+first_partition = ''.join(web_partition("<tbody>", "</tbody>", webpage_code));
+
+
+remove_array = ['<tbody>', '<tr>', '<td  >', '</td> ', "<td class=' latlon' >", "<td class=' mag' > ", "<td class=' dep' > ", "<td class=' region' > <a href='"
+                "<TD class='evid'>&nbsp;&nbsp;<a href="]
+strip_data = clean_data(remove_array, first_partition);
+strip_data = re.sub('\n<td class=.*?</tr> ', '', strip_data, flags=re.DOTALL)
+
+#remove newlines
+print(strip_data)
