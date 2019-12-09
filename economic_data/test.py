7c5a71df76eb0eec8448a9f9ce9ced1428ba4523
StefanPowell <Stefan.Powell001@umb.edu>
2019-12-09 18:41:40 -0500

    update code

--- economic_data/test.py
+++ economic_data/test.py
@@ -8,6 +8,7 @@ with urllib.request.urlopen("https://cdn-nfs.faireconomy.media/ff_calendar_thisw
 
 data_today = []
 high_impact = []
+Economicdata = []
 
 def today_data():
     for x in range(len(data)):
@@ -17,20 +18,64 @@ def today_data():
         date = datetime[0]
         time = datetime[1]
         dateStr = dateTimeObj.strftime("%Y-%m-%d")
-        if(str(date) == dateStr):
-            data_today.append(data_dict)
+
+        #if(str(date) != dateStr):
+
+        data_today.append(data_dict)
     return data_today
 
-def impact(data):
+def impact(level, data):
     for x in range(len(data)):
         data_dict = data[x]
         impact = data_dict["impact"]
-        if(impact == "Medium"):
+        if(impact == level):
             high_impact.append(data_dict)
     return high_impact
 
+def start():
+    #focus on high impact
+    get_data_today = today_data()
+    high_impact_news = impact("High", get_data_today)
+    for y in high_impact_news:
+        if(y['forecast'] == ''):
+            continue
+        else:
+            datetime = y["date"]
+            datetime = datetime.split("T")
+            time = datetime[1].split("-")
+            time = time[0]
+            print(time)
 
-print(today_data())
+
+            #print(y['title'])
+            #print(y['forecast'])
+            #print(y['previous'])
+            #print(y['country'])
+            #print(y['date'])
+
+start()
+
+class _currency_:
+    def __init__(self, symbol, country):
+        self.symbol = symbol
+        self.country = country
+
+class economic_data:
+    def __init__(self, name, currency, direction, effect):
+        self.name = name
+        self.currency = currency
+        self.direction = direction
+        self.effect = effect
+        Economicdata.append(self)
+
+
+def create_economic_data():
+    economic_data("German ZEW Economic Sentiment", "EUR", "up", "positive")
+
+
+create_economic_data()
+#for data in Economicdata:
+#    print(data.name)
 
 
 
